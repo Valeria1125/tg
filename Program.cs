5a1518f095715b1ad661950d2785405b4cc6d77d
Lera <73580648+Valeria1125@users.noreply.github.com>
2022-04-28 16:44:59 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace tg
+{
+    class Program
+    {
+        private static string token { get; set; } = "5222380705:AAGFM8dLcNyS2o2CN3BJ8vA0WHavtddgtM8";
+
+        private static TelegramBotClient client;
+
+        static void Main(string[] args)
+        {
+            client = new TelegramBotClient(token);
+
+            client.StartReceiving();
+
+            client.OnMessage += OnMassegeHandler;
+
+            Console.ReadLine();
+
+            client.StopReceiving();
+        }
+
+        private static async void OnMassegeHandler(object sender, MessageEventArgs e)
+        {
+            var msg = e.Message;
+            if (msg.Text != null) ;
+            {
+                Console.WriteLine($"Новое сообщение:{msg.Text}");
+                await client.SendTextMessageAsync(msg.Chat.Id, msg.Text, replyToMessageId: msg.MessageId);
+            }
+        }
+    }
+}
